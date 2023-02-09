8d58b7c7b5446cc879d45402b831db80d6eb3f95
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-02-09 18:00:39 -0300

    refactor: simplify code SungUpController

--- src/presentation/controllers/singUp.ts
+++ src/presentation/controllers/singUp.ts
@@ -2,10 +2,12 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-params-errors'
 import { badRequest } from '../helpers/http-helpers'
 export class SingUpController {
-  handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'));
+  handle (httpRequest: HttpRequest): HttpResponse | any {
+    const requireFields = ['name', 'email'];
+    for (const field of requireFields) {
+      if (!httpRequest[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
-    return badRequest(new MissingParamError('email'));
   }
 }
