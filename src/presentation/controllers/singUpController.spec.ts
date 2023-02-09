315f4c43c9cd7fd657346eaf6818ef619f174924
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-02-09 18:11:05 -0300

    test: ensure SingUpController returns 400 if no password is provided

--- src/presentation/controllers/singUpController.spec.ts
+++ src/presentation/controllers/singUpController.spec.ts
@@ -2,7 +2,7 @@ import { SingUpController } from './singUp'
 import { MissingParamError } from '../errors/missing-params-errors'
 
 describe('SignUpController', () => {
-  test('Should return 400 if no name is provided', () => {
+  it('Should return 400 if no name is provided', () => {
     // sut -> system under test
     const sut = new SingUpController()
     const httpRequest = {
@@ -13,12 +13,11 @@ describe('SignUpController', () => {
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    console.log('error', httpResponse.body);
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse?.body).toEqual(new MissingParamError('name'))
   })
 
-  test('Should return 400 if no email is provided', () => {
+  it('Should return 400 if no email is provided', () => {
     // sut -> system under test
     const sut = new SingUpController()
     const httpRequest = {
@@ -29,8 +28,22 @@ describe('SignUpController', () => {
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    console.log('error', httpResponse.body);
     expect(httpResponse.statusCode).toBe(400)
-    // expect(httpResponse.body).toEqual(new MissingParamError('email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  it('Should return 400 if no password is provided', () => {
+    // sut -> system under test
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        name: 'name.any',
+        email: 'email@gmail.com',
+        passwordValidate: 'password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 })
