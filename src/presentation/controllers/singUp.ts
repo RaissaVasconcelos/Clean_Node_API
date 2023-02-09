0d14cdc04f3bad54aab3247058bd13cfd41982ae
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-02-09 14:56:39 -0300

    feat: add message error

--- src/presentation/controllers/singUp.ts
+++ src/presentation/controllers/singUp.ts
@@ -1,5 +1,8 @@
 export class SingUpController {
   handle (httpRequest: any): any {
-    return {}
+    return {
+      statusCode: 400,
+      body: new Error('Missing: param: name')
+    }
   }
 }
