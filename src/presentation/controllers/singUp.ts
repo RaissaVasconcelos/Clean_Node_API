ef6239f80cc3235cc89958fd01f2eb6b1a252f5f
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-02-21 12:10:06 -0300

    feat: ensure SingUpController calls AddAccount with correct values

--- src/presentation/controllers/singUp.ts
+++ src/presentation/controllers/singUp.ts
@@ -1,31 +1,40 @@
 import { HttpResponse, HttpRequest, Controller, EmailValidator } from '../protocols'
 import { badRequest, serverError } from '../helpers/http-helpers'
 import { InvalidParamError, MissingParamError } from '../errors'
+import { AddAccount } from '../../domain/usecases/add-account'
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailIsValidate: EmailValidator) {
+  constructor (emailIsValidate: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailIsValidate
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse | any {
     try {
       const requireFields = ['name', 'email', 'password', 'passwordValidate'];
+
       for (const field of requireFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field));
         }
       }
       
-      if (httpRequest.body.password !== httpRequest.body.passwordValidate) {
+      const { name, email, password, passwordValidate } = httpRequest.body;
+
+      if (password !== passwordValidate) {
         return badRequest(new InvalidParamError('passwordValidate'))
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
       
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      this.addAccount.add({ name, email, password })
+
     } catch (error) {
       return serverError()
     }
